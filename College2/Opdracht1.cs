3b76b4eeef374ba74ab0ac518f3cb83efb19621f
Lucas Doddema <lucas.doddema@outlook.com>
2023-09-14 14:18:54 +0200

    College 2 opdracht 1

--- College2/Opdracht1.cs
+++ College2/Opdracht1.cs
@@ -1,6 +1,64 @@
 ﻿namespace C_Sharp_opdrachten.College2;
 
-public class Opdracht1
-{
-    
+// Enumerations voor slot en kleur
+public enum SlotType {
+    GeenSlot,
+    Kabelslot,
+    Kettingslot
+}
+
+public enum Kleur {
+    Rood,
+    Blauw,
+    Groen
+}
+
+// Class Bicycle
+public class Bicycle {
+
+    // Properties
+    public SlotType Slot { get; set; }
+    public int FrameHoogte { get; set; }
+    public Kleur Kleur { get; set; }
+
+    // Status variabele
+    private bool IsGesloten { get; set; }
+
+    // Constructor
+    public Bicycle(SlotType slot, int framehoogte, Kleur kleur) {
+        this.Slot = slot;
+        this.FrameHoogte = framehoogte;
+        this.Kleur = kleur;
+        this.IsGesloten = false;
+    }
+
+    // Methodes
+    public void Lock() {
+        this.IsGesloten = true;
+    }
+
+    public void Unlock() {
+        this.IsGesloten = false;
+    }
+
+    // ToString() methode
+    public override string ToString() {
+        return $"Slot: {this.Slot} | Framehoogte: {this.FrameHoogte} | Kleur: {this.Kleur} | IsGesloten: {this.IsGesloten}";
+    }
+}
+
+public class Program {
+
+    public static void Execute() {
+
+        // Creëer drie Bicycle objecten
+        Bicycle racefiets = new Bicycle(SlotType.Kabelslot, 58, Kleur.Rood);
+        Bicycle mountainbike = new Bicycle(SlotType.Kettingslot, 48, Kleur.Blauw);
+        Bicycle stadsfiets = new Bicycle(SlotType.GeenSlot, 56, Kleur.Groen);
+
+        // Print de drie Bicycle objecten
+        Console.WriteLine(racefiets);
+        Console.WriteLine(mountainbike);
+        Console.WriteLine(stadsfiets);
+    }
 }
---
+++ Mian.cs
@@ -0,0 +1,25 @@
+﻿namespace C_Sharp_opdrachten;
+
+public class Mian
+{
+    public static void Main(string[] args)
+    {
+        // --------------- college 1 ------------------
+        //College1.Opdracht1.Execute();
+        //College1.Opdracht2.Execute();
+        //College1.Opdracht3.Execute();
+        //College1.Opdracht4.Execute();
+        // College1.Opdracht5.Execute();
+        // College1.Opdracht6.Execute();
+        // College1.Opdracht7.Execute();
+        // College1.Opdracht8.Execute();
+        // College1.Opdracht9.Execute();
+        // College1.Opdracht10.Execute();
+        
+        // --------------- college 2 ------------------
+        College2.Program.Execute();
+
+
+
+    }
+}
